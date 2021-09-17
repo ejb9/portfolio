dc883ba9755d987c658d2a81e0539112d4585d48
Emily Braudaway <Emily.Braudaway@sckans.edu>
2021-09-17 14:56:34 -0500

    edit

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,19 +61,19 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Emily <span className="text-color-primary">Braudaway</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+              Go Moundbuilders!
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                    About Me
                     </Button>
                   <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
+                    Contact Me
                     </Button>
                 </ButtonGroup>
               </div>
---
+++ src/views/EasterEgg.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const EasterEgg = () => {
+    return (
+        <div class="easter-egg-avocado">
+            Whatever you want
+            It was the bst of times
+            <div class="inner-avocado">
+                <div class="inner-inner-avocado">
+                    Inner-inner div
+                </div>
+                avocado
+                <div class="other-inner-inner-avocado">
+                    Other inner-inner div
+                </div>
+                <div class="other-inner-inner-avocado">
+                    Other-inner-inner div
+                </div>
+            </div>
+        </div>
+    );
+};
